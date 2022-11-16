e3aecd56c7ddf56c18bb2139219d91143d8db046
Jorge Torres <jtorres1@ucol.mx>
2022-11-15 19:42:45 -0600

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
       {console.log(userData)}
       <nav className=' navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow-lg px-4 '>
         <div className='container-fluid'>
-          <Link className='navbar-brand' to='/'>Navbar</Link>
+          <Link className='navbar-brand' to='/'>E-commerce</Link>
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon' />
           </button>
